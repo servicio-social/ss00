06e41778b59e3078a32629bcc03275850d793c43
Eduardo Félix <eduardofelix@zn00.net>
2015-05-06 17:54:19 -0600

    Welcome Blade

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <html>
 	<head>
-		<title>Laravel</title>
+		<title>Servicio Social</title>
 		
 		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
 
@@ -40,7 +40,7 @@
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">Laravel 5</div>
+				<div class="title">Servicio Social</div>
 				<div class="quote">{{ Inspiring::quote() }}</div>
 			</div>
 		</div>

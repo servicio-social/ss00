b1970aa3eeef23331e26fd042ff8995e9028e71c
Eduardo Félix <eduardofelix@zn00.net>
2015-06-01 23:43:11 -0600

    Modelo de Documento

--- laravel/app/Documento.php
+++ laravel/app/Documento.php
@@ -7,8 +7,8 @@ class Documento extends Eloquent {
 	protected $table = 'documentos';
 	protected $fillable = ['cn', 'formato', 'comentario', 'status', 'nombre', 'archivo', 'tipo'];
     
-    public function User(){
-        return $this->belongsTo('User');
+    public function UserInfo(){
+        return $this->belongsTo('UserInfo');
     }
 
 }
